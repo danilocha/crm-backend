5cce28462e5d35f4ed7ef56078b3029072e812db
danilocha <danilocha1@gmail.com>
2019-12-21 19:33:23 -0500

    arreglo errores

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ mongoose.connect(process.env.DB_URL, {
 });
 // crear el servidor
 const app = express();
+// carpeta publica
+app.use(express.static("uploads"));
 
 //habilitar el bodyparser
 app.use(bodyParser.json());
@@ -37,8 +39,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use("/", routes());
-// carpeta publica
-app.use(express.static("uploads"));
 
 const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 5000;
